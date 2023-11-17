a0debe6e2e80cd6e37ccd2e0352ddfb59d26a757
SMR <mohammad.rouzegar78@gmail.com>
2023-11-17 20:09:19 +0330

    changes

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from project.controllers.main_controller import (
 
 if __name__ == '__main__':
     MainController(
-        bench_file_name='bench_files/c432.bench',
-        input_file_name='input_files/c432.txt',
-        test_input_file_name='test_input_files/c432.txt',
-        true_value_result_file_name='result_files/true_value_simulation_c432.txt',
-        fault_simulation_result_file_name='result_files/deductive_fault_simulation_c432.txt'
+        bench_file_name='bench_files/c7552.bench',
+        input_file_name='input_files/c7552.txt',
+        test_input_file_name='test_input_files/c7552.txt',
+        true_value_result_file_name='result_files/true_value_simulation_c7552.txt',
+        fault_simulation_result_file_name='result_files/deductive_fault_simulation_c7552.txt'
     ).run()
